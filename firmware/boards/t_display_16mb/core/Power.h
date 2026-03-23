39ba3a6e2d73ca5df912c9697a3b98ea6e422539
lshaf <lshaf@users.noreply.github.com>
2026-03-23 09:54:13 +0700

    💉 fix t_display

--- firmware/boards/t_display_16mb/core/Power.h
+++ firmware/boards/t_display_16mb/core/Power.h
@@ -10,11 +10,11 @@ public:
   void begin() override {
     pinMode(ADC_EN, OUTPUT);
     digitalWrite(ADC_EN, HIGH);
-    pinMode(ANALOG_BAT_PIN, INPUT);
+    pinMode(BAT_ADC_PIN, INPUT);
   }
 
   uint8_t getBatteryPercentage() override {
-    float mv = (float)analogReadMilliVolts(ANALOG_BAT_PIN) * 2.0f;
+    float mv = (float)analogReadMilliVolts(BAT_ADC_PIN) * 2.0f;
     float pct = (mv - 3300.0f) / (4150.0f - 3350.0f) * 100.0f;
     if (pct < 0.0f)   pct = 0.0f;
     if (pct > 100.0f) pct = 100.0f;
